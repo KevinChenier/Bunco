c975fd3f789acb16028ed12abfcba5694e30cfa9
LaurentSHub <laurentsieu@gmail.com>
2019-03-08 23:43:34 -0500

    grosse marde . avec effort

--- Code/src/Test/JoueurEtTeste.java
+++ Code/src/Test/JoueurEtTeste.java
@@ -39,11 +39,44 @@ class JoueurEtTeste {
 	}
 	//Teste des scores
 	@Test	
-	public void scoresJoueurTest() {
-		int score = 88;
+	public void setScoresJoueurTest() {
+		int score = 8;
 		Joueur personne = new Joueur(debut);
 		personne.setScore(score);
-		assertEquals(joueur. )
+		assertEquals(personne.getScore(),score );	 
+	}
+	public void getScoresJoueurTest() {
+		int score = 8;
+		Joueur personne = new Joueur(debut);
+		personne.setScore(score);
+		assertEquals(personne.getScore(),score);
+	}
+	//compareTo
+	public void compareToEgalTest() {
+		int scorePareil = 11;
+		Joueur personne1 = new Joueur(1);
+		Joueur personne2 = new Joueur(2);
+		personne1.setScore(scorePareil);
+		personne2.setScore(scorePareil);
+		assertEquals(personne1.compareTo(personne2),0);
+	}
+	public void compareToInferieur() {
+		int scoreSup = 2;
+		int scoreInf = 1;
+		Joueur personne1 = new Joueur(1);
+		Joueur personne2 = new Joueur(2);
+		personne1.setScore(scoreInf);
+		personne2.setScore(scoreSup);
+		assertEquals(-1,personne1.compareTo(personne2));
+	}
+	public void compareToSuperieur() {
+		int scoreSup = 3;
+		int scoreInf = 2;
+		Joueur personne1 = new Joueur(1);
+		Joueur personne2 = new Joueur(2);
+		personne1.setScore(scoreInf);
+		personne2.setScore(scoreSup);
+		assertEquals(1,personne1.compareTo(personne2));
 	}
 	//TODO FONCTIONNALITE DE BASE APPEL NORMAL ET TYPIQUE ?
 	
